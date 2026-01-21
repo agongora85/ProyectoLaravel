d5336b6778d92e024ce2366c22731ed3118d33d8
Alejandro Gongora <alejandro.gongora@pjyucatan.gob.mx>
2026-01-21 14:29:28 -0600

    Se agrega el comentario

--- database/migrations/2026_01_21_192403_create_posts_table.php
+++ database/migrations/2026_01_21_192403_create_posts_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        /** Para revertir o eliminar la tabla posts */
         Schema::dropIfExists('posts');
     }
 };
