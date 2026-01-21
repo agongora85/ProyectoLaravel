5c9d6009ea6a6202e756e41712a96179b368d96e
Alejandro Gongora <alejandro.gongora@pjyucatan.gob.mx>
2026-01-21 14:25:00 -0600

    Se modifica el archivo de migrate para que el campo avatar me lo ponga despues de name

--- database/migrations/2026_01_21_195459_add_avatar_to_users_table.php
+++ database/migrations/2026_01_21_195459_add_avatar_to_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->string('avatar')->nullable();
+            $table->string('avatar')->nullable()->after('name');
         });
     }
 
