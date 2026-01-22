328658292b85a3b8a7056ff8a77f2fb10c1ea0af
Alejandro Gongora <alejandro.gongora@pjyucatan.gob.mx>
2026-01-22 14:37:36 -0600

    Se incorpora la libreria de fontawesome y se modifica la plantilla para incorporar los iconos de las redes sociales

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+    />
   <style>
   .fakeimg {
     height: 200px;
@@ -84,6 +87,20 @@
 
 <div class="mt-5 p-4 bg-dark text-white text-center">
   <p>Footer</p>
+  <i class="fa-brands fa-facebook"></i>
+  <i class="fa-brands fa-twitter"></i>
+  <i class="fa-brands fa-instagram"></i>
+  <i class="fa-brands fa-linkedin"></i>
+  <i class="fa-brands fa-github"></i>
+  <i class="fa-brands fa-whatsapp"></i>
+  <i class="fa-brands fa-telegram"></i>
+  <i class="fa-brands fa-youtube"></i>
+  <i class="fa-brands fa-twitch"></i>
+  <i class="fa-brands fa-discord"></i>
+  <i class="fa-brands fa-snapchat"></i>
+  <i class="fa-brands fa-pinterest"></i>
+  <i class="fa-brands fa-reddit"></i>
+  <i class="fa-brands fa-tiktok"></i>
 </div>
 
 </body>
